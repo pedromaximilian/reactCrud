39533bdae42c50e7e4a07b027f322ed1c92a8966
pedromaximilian <pedro.maximilian@gmail.com>
2021-05-17 04:07:34 -0300

    add task

--- src/App.js
+++ src/App.js
@@ -1,12 +1,74 @@
-
+import React, { useState } from 'react'
+import {isEmpty} from 'lodash'
+import shortid from 'shortid'
 
 function App() {
+  const [task, setTask] = useState("")
+  const [tasks, setTasks] = useState([])
+
+  const addTask = (e) => {
+    e.preventDefault();
+    if (isEmpty(task)) {
+      console.log("Empty")
+      return
+    }
+
+
+    const newTask = {
+      id: shortid.generate(),
+      name: task
+    }
+
+    setTasks([...tasks, newTask])
+    setTask("")
+    
+  }
   return (
-   <div className="container"> 
-   
-     <h1>Tareas</h1>
-     <hr/>
-   </div>
+    <div className="container">
+      <h1>Tareas</h1>
+      <hr />
+      <div className="row">
+        <div className="col-8">
+          <h4 className="text-center">Lista de Tareas</h4>
+          <ul className="list-group">
+            {
+              tasks.map((task) => (
+                <li className="list-group-item" key={task.id}>
+                <span className="lead">{task.name} </span>
+                <button className="mx-2 btn btn-danger btn-sm float-right">
+                  Eliminar{" "}
+                </button>
+                <button className="btn btn-warning btn-sm float-right">
+                  Edit{" "}
+                </button>
+              </li>
+
+              ))
+
+
+
+            }
+
+            
+          </ul>
+        </div>
+        <div className="col-4">
+          <h4 className="text-center"> Formulario</h4>
+          <form onSubmit={addTask}>
+            <input
+              type="text"
+              className="form-control mb-2"
+              placeholder="Ingrese la tarea..."
+              onChange={(text) => setTask(text.target.value)}
+              value={task}
+            />
+            <button type="submit" className="btn btn-dark btn-block">
+              Agregar
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
 
